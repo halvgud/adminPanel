9e8ab8d4fefacf5aa621128bb7f85969ca769bb3
Edwin Garcia <ed@live.no>
2020-04-15 23:52:22 -0500

    color setup

--- config/voyager.php
+++ config/voyager.php
@@ -182,7 +182,7 @@ return [
     |
     */
 
-    'primary_color' => '#22A7F0',
+    'primary_color' => '#592d86',
 
     'show_dev_tips' => true, // Show development tip "How To Use:" in Menu and Settings
 
